4f9a4ef0dc61c07c51b4fd45cd86b5c111d0a854
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 21:14:23 +0500

    done

--- book_lendings/serializers.py
+++ book_lendings/serializers.py
@@ -1,8 +1,10 @@
+from django.utils import timezone
 from rest_framework import serializers
-from .models import BookLending, BookCopy
-
+from .models import BookLending
 
 class BookLendingSerializer(serializers.ModelSerializer):
+    days_remaining = serializers.SerializerMethodField()
+
     class Meta:
         model = BookLending
         fields = (
@@ -14,20 +16,28 @@ class BookLendingSerializer(serializers.ModelSerializer):
             'due_date',
             'returned_date',
             'status',
+            'days_remaining',
         )
 
+    def get_days_remaining(self, obj):
+        due_date = obj.due_date
+        if due_date:
+            remaining_days = (due_date - timezone.now()).days
+            return remaining_days
+        return None
 
-class BookLendingListSerializer(BookLendingSerializer):
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         book_copy = instance.book_copy
         rep['book_copy'] = {
             'id': book_copy.id,
-            'inventory_number': book_copy.inventory_number,
             'book': {
                 'id': book_copy.book.id,
                 'title': book_copy.book.title,
-                'isbn': book_copy.book.isbn
-            }
+                'isbn': book_copy.book.isbn,
+                'authors': [{'id': author.id, 'first_name': author.first_name, 'last_name': author.last_name} for author in book_copy.book.authors.all()]
+            },
+            'inventory_number': book_copy.inventory_number,
+            'condition': book_copy.condition,
         }
         return rep
--- book_lendings/views.py
+++ book_lendings/views.py
@@ -1,22 +1,14 @@
 from rest_framework import generics
 from .models import BookLending
-from .serializers import BookLendingListSerializer, BookLendingSerializer
+from .serializers import BookLendingSerializer
 from .pagination import BookLendingPagination
 
 class BookLendingListCreateView(generics.ListCreateAPIView):
     queryset = BookLending.objects.all()
     pagination_class = BookLendingPagination
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return BookLendingListSerializer
-        return BookLendingSerializer
+    serializer_class = BookLendingSerializer
 
 
 class BookLendingRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = BookLending.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return BookLendingListSerializer
-        return BookLendingSerializer
+    serializer_class = BookLendingSerializer
