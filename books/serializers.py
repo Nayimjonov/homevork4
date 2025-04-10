9d7b8caf828ff46d2e00628bf49730000b68ffe4
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 10:14:47 +0500

    done

---
+++ books/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Book
+from authors.models import Author
+from genres.models import Genre
+
+
+class BookAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'first_name', 'last_name')
+
+
+class BookGenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            'id',
+            'title',
+            'authors',
+            'genre',
+            'isbn',
+            'published_date',
+            'description',
+            'page_count',
+            'language',
+            'copies_available'
+        )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['authors'] = BookAuthorSerializer(instance.authors, many=True).data
+        rep['genre'] = BookGenreSerializer(instance.genre).data
+        return rep
