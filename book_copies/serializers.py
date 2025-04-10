b9fa98fc0a17377e7071f1f16d3bac63b8cba83f
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 12:09:53 +0500

    don e serialzier

--- book_copies/serializers.py
+++ book_copies/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
 from .models import BookCopy
+from books.models import Book
 
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'isbn')
 
 class BookCopySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookCopy
+        fields = ('id', 'book', 'inventory_number', 'condition', 'is_available', 'added_date')
+
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['book'] = BookSerializer(instance.book).data
+        return rep
