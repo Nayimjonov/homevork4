81bee1b77dbe95cca6c88da8c325c6a74a7f3cc0
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 20:25:47 +0500

    done

---
+++ book_lendings/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import BookLending
+
+
+class BookLendingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookLending
+        fields = (
+            'id',
+            'book_copy',
+            'borrower_name',
+            'borrower_email',
+            'borrowed_date',
+            'due_date',
+            'returned_date',
+            'status',
+        )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        book_copy = instance.book_copy
+        rep['book_copy'] = {
+            'id': book_copy.id,
+            'inventory_number': book_copy.inventory_number,
+            'book': {
+                'id': book_copy.book.id,
+                'title': book_copy.book.title,
+                'isbn': book_copy.book.isbn
+            }
+        }
+        return rep
