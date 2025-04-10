c7295c16a481a873a909f34e72c0543b30d53843
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 21:26:42 +0500

    all done

--- book_lendings/urls.py
+++ book_lendings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('lendings/', views.BookLendingListCreateView.as_view(), name='list'),
     path('lendings/<int:pk>/', views.BookLendingRetrieveUpdateAPIView.as_view(), name='lendings_detail'),
+    path('lendings/<int:pk>/return/', views.BookLendingReturnView.as_view(), name='book_lending_return'),
+    path('lendings/<int:pk>/overdue/', views.BookLendingOverdueView.as_view(), name='book_lending_overdue'),
 ]
--- book_lendings/views.py
+++ book_lendings/views.py
@@ -1,14 +1,42 @@
-from rest_framework import generics
+from django.utils import timezone
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BookLending
 from .serializers import BookLendingSerializer
 from .pagination import BookLendingPagination
+
 
 class BookLendingListCreateView(generics.ListCreateAPIView):
     queryset = BookLending.objects.all()
     pagination_class = BookLendingPagination
     serializer_class = BookLendingSerializer
 
-
 class BookLendingRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = BookLending.objects.all()
     serializer_class = BookLendingSerializer
+
+class BookLendingReturnView(generics.RetrieveAPIView):
+    queryset = BookLending.objects.all()
+    serializer_class = BookLendingSerializer
+
+    def get(self, request, *args, **kwargs):
+        lending = self.get_object()
+        if lending.status == 'returned':
+            return Response({"detail": "This book is already returned."}, status=status.HTTP_400_BAD_REQUEST)
+        lending.status = 'returned'
+        lending.returned_date = timezone.now()
+        lending.save()
+
+        return Response(self.get_serializer(lending).data, status=status.HTTP_200_OK)
+
+class BookLendingOverdueView(generics.RetrieveAPIView):
+    queryset = BookLending.objects.all()
+    serializer_class = BookLendingSerializer
+
+    def get(self, request, *args, **kwargs):
+        lending = self.get_object()
+        if lending.due_date < timezone.now() and lending.status != 'returned':
+            lending.status = 'overdue'
+            lending.save()
+
+        return Response(self.get_serializer(lending).data, status=status.HTTP_200_OK)
